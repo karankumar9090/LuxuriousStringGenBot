ba7c3305563bdcc1c3c966bb4bd5138faf89cec1
AimmXD <storejyan@gmail.com>
2024-04-23 01:08:36 +0530

    Initial Commit

---
+++ AIMXSTRINGBOT/start.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""Hᴇʏ {msg.from_user.mention},
+
+I Aᴍ {me2},
+Tʀᴜsᴛᴇᴅ sᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ.
+Fᴜʟʟʏ sᴀғᴇ & sᴇᴄᴜʀᴇ.
+Nᴏ ᴇʀʀᴏʀ.""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Gᴇɴᴇʀᴀᴛᴇ Sᴛʀɪɴɢ", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton(" Sᴜᴘᴘᴏʀᴛ", url="https://t.me/AimXsupport"),
+                    InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/AimXbots")
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
