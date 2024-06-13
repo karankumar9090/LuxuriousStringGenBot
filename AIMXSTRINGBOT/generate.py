bf211f0a1358a6da6eb4fefd36d5aa6c53ed7cc9
karankumar9090 <149555639+karankumar9090@users.noreply.github.com>
2024-06-13 19:32:37 +0530

    Update generate.py

--- AIMXSTRINGBOT/generate.py
+++ AIMXSTRINGBOT/generate.py
@@ -174,7 +174,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
         string_session = client.session.save()
     else:
         string_session = await client.export_session_string()
-    text = f"**Tʜɪs ɪs ʏᴏᴜʀ {ty} sᴛʀɪɴɢ sᴇssɪᴏɴ** \n\n`{string_session}` \n\n**Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ :** @LuxuriousStringGenBot\n🍒 **𝖭𝖮𝖳𝖤 :** ᴅᴏɴ'ᴛ sʜᴀʀᴇ ɪᴛ ᴡɪᴛʜ ʏᴏᴜʀ ɢɪʀʟғʀɪᴇɴᴅ."
+    text = f"**Tʜɪs ɪs ʏᴏᴜʀ {ty} sᴛʀɪɴɢ sᴇssɪᴏɴ** \n\n`{string_session}` \n\n**Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ :** @LuxuriousStringsGenBot\n🍒 **𝖭𝖮𝖳𝖤 :** ᴅᴏɴ'ᴛ sʜᴀʀᴇ ɪᴛ ᴡɪᴛʜ ʏᴏᴜʀ ɢɪʀʟғʀɪᴇɴᴅ."
     try:
         if not is_bot:
             await client.send_message("me", text)
